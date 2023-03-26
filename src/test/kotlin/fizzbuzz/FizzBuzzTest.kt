72b5a1ac277464bf265b5de554f939b156082917
Thiago Santos <thiago.santos@tvyjwcy6kj.fritz.box>
2023-03-26 17:59:04 +0200

    Refactor Fizz and Buzz unit tests to a unique test

--- src/test/kotlin/fizzbuzz/FizzBuzzTest.kt
+++ src/test/kotlin/fizzbuzz/FizzBuzzTest.kt
@@ -8,16 +8,7 @@ import kotlin.test.assertEquals
 class FizzBuzzTest {
 
     @Test
-    fun `Given numbers from 1 to 5 Then Replace all multiple of 3 to Fizz`() {
-        val fizzBuzz = FizzBuzz { listOf(1, 2, 3, 4, 5) }
-
-        val result = fizzBuzz.execute()
-
-        assertEquals(expected = "1\n2\nFizz\n4\nBuzz", actual = result)
-    }
-
-    @Test
-    fun `Given numbers from 1 to 5 Then Replace all multiple of 5 to Buzz`() {
+    fun `Given numbers from 1 to 5 Then Replace all multiple of 3 to Fizz and all multiples of 5 to Buzz`() {
         val fizzBuzz = FizzBuzz { listOf(1, 2, 3, 4, 5) }
 
         val result = fizzBuzz.execute()

189c2a61f77cbf657e386c16175e37d001408650
Thiago Santos <thiago.santos@tvyjwcy6kj.fritz.box>
2023-03-26 17:47:56 +0200

    Impement Fizz check algorithm

--- src/main/kotlin/fizzbuzz/FizzBuzz.kt
+++ src/main/kotlin/fizzbuzz/FizzBuzz.kt
@@ -3,6 +3,8 @@ package fizzbuzz
 class FizzBuzz {
 
     fun execute(): String {
-        return ""
+        val input = (1..100).map { if (it % 3 == 0) "Fizz" else it }
+
+        return input.joinToString("\n")
     }
 }

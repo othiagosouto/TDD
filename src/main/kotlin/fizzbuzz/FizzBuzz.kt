a5ee667b480357cc0d03d89212353a1cef59a524
Thiago Santos <thiago.santos@tvyjwcy6kj.fritz.box>
2023-03-26 18:20:58 +0200

    Use helper to check if number is Buzz

--- src/main/kotlin/fizzbuzz/FizzBuzz.kt
+++ src/main/kotlin/fizzbuzz/FizzBuzz.kt
@@ -3,7 +3,7 @@ package fizzbuzz
 class FizzBuzz(private val helper: FizzBuzzHelper, private val fetchNumbers: () -> List<Int>) {
 
     fun execute(): String {
-        val input = fetchNumbers().map { if (helper.isFizz(it)) "Fizz" else if (it % 5 == 0) "Buzz" else it }
+        val input = fetchNumbers().map { if (helper.isFizz(it)) "Fizz" else if (helper.isBuzz(it)) "Buzz" else it }
 
         return input.joinToString("\n")
     }

c5efc4eab24b5822190a8dacd7b71ab2e163421c
Thiago Santos <thiago.santos@tvyjwcy6kj.fritz.box>
2023-03-26 18:22:16 +0200

    Refactor FizzBuzz

--- src/main/kotlin/fizzbuzz/FizzBuzz.kt
+++ src/main/kotlin/fizzbuzz/FizzBuzz.kt
@@ -3,7 +3,13 @@ package fizzbuzz
 class FizzBuzz(private val helper: FizzBuzzHelper, private val fetchNumbers: () -> List<Int>) {
 
     fun execute(): String {
-        val input = fetchNumbers().map { if (helper.isFizz(it)) "Fizz" else if (helper.isBuzz(it)) "Buzz" else it }
+        val input = fetchNumbers().map {
+            when {
+                helper.isFizz(it) -> "Fizz"
+                helper.isBuzz(it) -> "Buzz"
+                else -> it
+            }
+        }
 
         return input.joinToString("\n")
     }

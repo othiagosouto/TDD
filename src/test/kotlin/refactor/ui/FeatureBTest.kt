5ffeb32a0cbc76c61f9f1d16ed3236cf298af534
Thiago Santos <thiago.santos@tvyjwcy6kj.fritz.box>
2023-03-26 20:49:44 +0200

    Add scenario for FeatureBTest

--- src/test/kotlin/refactor/ui/FeatureBTest.kt
+++ src/test/kotlin/refactor/ui/FeatureBTest.kt
@@ -2,6 +2,7 @@ package refactor.ui
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import refactor.domain.Label
 
 class FeatureBTest {
     private val mapper = LabelMapper()
@@ -14,6 +15,21 @@ class FeatureBTest {
         val result = featureB.getUiLabel(label = null)
 
         assertNull(result)
+    }
+
+    @Test
+    fun `Given label with null text-color Then returns label with default colors`() {
+        val label: Label? = Label(text = "text", backgroundColor = "red", textColor = null)
+
+        val result = mapper.toModel(label, false)
+
+        kotlin.test.assertEquals(
+            expected = UiLabel(
+                text = "text",
+                backgroundColor = "white",
+                textColor = "black"
+            ), actual = result
+        )
     }
 
 }

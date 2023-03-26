e4ce06c6ec5dbb407168052d78183f0f7993177d
Thiago Santos <thiago.santos@tvyjwcy6kj.fritz.box>
2023-03-26 18:18:48 +0200

    Make Buzz test pass

--- src/main/kotlin/fizzbuzz/FizzBuzzHelper.kt
+++ src/main/kotlin/fizzbuzz/FizzBuzzHelper.kt
@@ -1,8 +1,6 @@
 package fizzbuzz
 
 class FizzBuzzHelper {
-    fun isFizz(i: Int): Boolean = i%3 == 0
-    fun isBuzz(i: Int): Boolean {
-        TODO("Not yet implemented")
-    }
+    fun isFizz(i: Int): Boolean = i % 3 == 0
+    fun isBuzz(i: Int): Boolean = i % 5 == 0
 }

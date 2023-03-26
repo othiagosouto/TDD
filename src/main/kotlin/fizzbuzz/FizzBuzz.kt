dc01a20f94cb5a296ce6ea72ae78d67d183ef414
Thiago Santos <thiago.santos@tvyjwcy6kj.fritz.box>
2023-03-26 18:42:36 +0200

    Refactor FizzBuzz to use FizzBuzzHelper.isFizzBuzz

--- src/main/kotlin/fizzbuzz/FizzBuzz.kt
+++ src/main/kotlin/fizzbuzz/FizzBuzz.kt
@@ -5,7 +5,7 @@ class FizzBuzz(private val helper: FizzBuzzHelper, private val fetchNumbers: () 
     fun execute(): String {
         val input = fetchNumbers().map {
             when {
-                helper.isFizz(it) && helper.isBuzz(it) -> "FizzBuzz"
+                helper.isFizzBuzz(it) -> "FizzBuzz"
                 helper.isFizz(it) -> "Fizz"
                 helper.isBuzz(it) -> "Buzz"
                 else -> it
